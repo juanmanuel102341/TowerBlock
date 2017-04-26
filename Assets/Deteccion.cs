7a26cd660a8fff42fcabb578d2393fa5b150a748
juanmanuel102341 <jms24@live.com.ar>
2017-04-25 21:53:27 -0300

    juego tower block funciona lo basico
    click del mousse aparece cajas

---
+++ Assets/Deteccion.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deteccion : MonoBehaviour {
+	private GameObject camera_01;
+	public float velocityCamera;
+	private GameObject objSpawn;
+	private Transform transformSpawn;
+	// Use this for initialization
+	//public SpawnObjetos scriptObj; 
+	private Transform cameraTransform;
+	private bool active=false;
+	private float height;
+	void Start () {
+
+		camera_01=GameObject.FindGameObjectWithTag("camara");
+
+		objSpawn=GameObject.FindGameObjectWithTag("jugador");
+		transformSpawn=objSpawn.GetComponent<Transform>();
+		cameraTransform=camera_01.GetComponent<Transform>();
+		height=this.transform.localScale.y;
+		print("ancho "+height);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnCollisionEnter(Collision c){
+		if(c.collider.tag=="enemigo"&&active==false){
+			cameraTransform.Translate(Vector3.up*velocityCamera);
+			//transformSpawn.transform.position=new Vector3(transform.position.x,transform.position.y+height,transform.position.z);
+			transformSpawn.Translate(Vector3.up*velocityCamera);
+
+			print("contacto caja");
+			active=true;
+		}else{
+			if(c.collider.tag=="piso"&&SpawnObjetos.cantidadCajas!=1){
+				print("perdiste");
+			//	SpawnObjetos.gameOver=true;
+			}
+		}
+
+	}
+}
---
+++ Assets/SpawnObjetos.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObjetos : MonoBehaviour {
+	public GameObject obj;
+
+
+	public static bool gameOver=false;
+	public static int cantidadCajas=0;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//tiempo+=Time.deltaTime;
+
+		//print("cajas "+);
+		if(gameOver==false){
+		if(Input.GetKeyDown(KeyCode.Mouse0)){
+			
+
+			cantidadCajas+=1;
+			print("cantidad cajas "+cantidadCajas);
+			Instantiate(obj,transform.position,transform.rotation);
+
+		}
+		}else{
+
+		}
+			
+
+	}
+}
